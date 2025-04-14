0ec9fe143766c8f6ed3a5a6d3ec8aefdd9aec032
potichatmignon <arthur.garnier1090@gmail.com>
2025-04-14 19:57:54 +0200

    fix scroll bar

--- frontend/component/Terminal.jsx
+++ frontend/component/Terminal.jsx
@@ -219,7 +219,7 @@ $$$$$$$  |  $$ |   \\$$$$$$  |$$$$$$$$\\ $$$$$$\\ \\$$$$$$  |$$ | \\$$\\ $$$$$$$
     }, [history]);
 
     return (
-        <div className="bg-black text-white font-mono p-4 h-full w-full flex flex-col rounded-lg shadow-lg border border-gray-700">
+        <div className="bg-black text-white font-mono p-4 h-full w-full flex flex-col rounded-lg shadow-lg border border-gray-700 terminal">
             <div className="flex-1 text-[10px] overflow-y-auto mb-2">
                 {history.map((entry, index) => (
                     <div key={index}>
